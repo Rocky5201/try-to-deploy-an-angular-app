4eb546ec4c5f06f3f3c885ae706a4cb3620e591e
Cheng-chiang Chen <Chiang.Chen@pyramidci.com>
2023-07-07 14:23:16 -0400

    update-card-component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,5 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  content={
+    image:"../../assets/robot.jpg",
+    link:'https://icat.vt.edu/projects/2015-2016/major/escher-humanoid-robot.html',
+    topie:"Humanoid Robot",
+    action:"MORE",
+    paragraph:"Robot was intended to be entered into US Navycompetition to assist in firefighting sea rescues."
+  };
 
+  newWindow(){
+    window.open(this.content.link, '_blank' );
+  }
 }
